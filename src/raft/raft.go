ac9e4c6d7386df8dd9c8155e4dee6dda2da5f367
huangwei <sniperhw@163.com>
2019-11-05 10:14:56 +0800

    fixed

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -420,7 +420,7 @@ func (rf *Raft) AppendEntrys(args *RequestAppendEntrysArgs, reply *RequestAppend
 	rf.mu.Lock()
 	defer rf.mu.Unlock()
 
-	/*if args.LeaderId == rf.leader && args.PrevLogIndex <= rf.commitIndex {
+	/*if args.LeaderId == rf.leader && args.PrevLogIndex < rf.commitIndex {
 		//相同leader,entry已经commit,判定为已经处理过的包的复现
 		return
 	}*/
